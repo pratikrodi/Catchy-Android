6648efc3ade26352073479123dfdaba3f7c56dfa
prateekrodi <prateekrodi#gmail.com>
2024-09-01 02:09:33 +0530

    uploading images

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+**Images**
+![](../../Pictures/Screenshots/Screenshot (18).png)
+![](../../Pictures/Screenshots/Screenshot (19).png)
+![](../../Pictures/Screenshots/Screenshot (21).png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,21 @@
-// lib/main.dart
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
+import 'firebase_options.dart'; // Ensure this file is correctly generated and configured
 import 'splash_screen.dart'; // Import the SplashScreen class
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: 'AIzaSyDc_yQfSwBU8e-j2x9IdhpfdATSEJZz844',
-      authDomain: 'catchy-b68d8.firebaseapp.com',
-      projectId: 'catchy-b68d8',
-      storageBucket: 'catchy-b68d8.appspot.com',
-      messagingSenderId: '874843638794',
-      appId: '1:874843638794:android:9cca7ec01f60c6652ea3f3',
-    ),
-  );
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform, // Use this if using Firebase CLI configuration
+    );
+  } catch (e) {
+    print('Error initializing Firebase: $e');
+    // Handle initialization error (e.g., show an error message or exit the app)
+  }
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
